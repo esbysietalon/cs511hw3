11288adcb698aa89059ef09065c1c2e88f21f515
talonjoaquin <jtalon@stevens.edu>
2019-06-29 19:56:09 -0400

    added definitions to functions in env.erl that pass dialyzer analysis

--- env.erl
+++ env.erl
@@ -5,13 +5,16 @@
 
 -spec new()-> envType().
 new() ->
-    % complete
+    dict:new().
 
 -spec add(envType(),atom(),valType())-> envType().
 add(Env,Key,Value) ->
-    % complete
+    dict:append(Key, Value, Env).
 
 -spec lookup(envType(),atom())-> valType().
 lookup(Env,Key) -> 
-   % complete
+   case dict:is_key(Key, Env) of
+      true -> dict:find(Key, Env);
+	  false -> {bool, false}
+   end.
 
